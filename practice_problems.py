6850d0bcdb55a55a4f231223e63c9840c3d03213
Sylvia <33107227+QiMiracleQi@users.noreply.github.com>
2018-01-11 19:46:06 -0600

    Update practice_problems.py

--- practice_problems.py
+++ practice_problems.py
@@ -3,6 +3,9 @@
 from collections import defaultdict
 
 def count_appearances(elements):
+    '''
+    This is a function adding 1 to each element
+    '''
     element_counts_map = defaultdict(int)
     for element in elements:
         element_counts_map[element] = element_counts_map.get(element, 0) + 1
